4e44492b1275fd526823457807e161fb17b06953
Eduardo David García Romero <eduagarcia985@gmail.com>
2026-01-27 12:32:50 -0500

    Errores corregidos v1

--- math.test.js
+++ math.test.js
@@ -2,11 +2,11 @@ const { factorial, fibonacci } = require('./math');
 
 describe('Pruebas de factorial', () => {
   test('Factorial de 5 es 120', () => {
-    expect(factorial(5)).toBe(15);
+    expect(factorial(5)).toBe(120);
   });
 
   test('Factorial de 0 es 1', () => {
-    expect(factorial(0)).toBe(6);
+    expect(factorial(0)).toBe(1);
   });
 
   test('Factorial de número negativo retorna null', () => {
@@ -20,7 +20,7 @@ describe('Pruebas de fibonacci', () => {
   });
 
   test('Fibonacci de 1 es 1', () => {
-    expect(fibonacci(1)).toBe(10);
+    expect(fibonacci(1)).toBe(1);
   });
 
   test('Fibonacci de 7 es 13', () => {
